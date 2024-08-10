ec3b2bd2b28d9b75ccd7def70db367bf01b1d0f0
ZouariOmar <zouariomar20@gmail.com>
2024-08-10 23:48:19 +0100

    add resources

--- cmd/sassToCss.sh
+++ cmd/sassToCss.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+
+
 # Ensure the script receives exactly 2 arguments
-if [ "$#" -ne 2 ]; then
+if [ "$#" -ne 1 ]; then
   echo "Usage: $0 input_filename output_filename"
   exit 1
 fi
 
+#* Compile sass to css in watch mode
+sass --watch ../project/sass/$1.sass:../project/css/$1.css
+
 # Compile Sass to CSS
-sass --style expanded "../project/sass/$1" > "../project/css/$2"
+#sass --style expanded "../project/sass/$1" > "../project/css/$2"
